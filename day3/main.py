1a082f43b175e3de8000066a9e783ffb289aa253
khdrvss <saidxon3105@gmail.com>
2025-12-17 00:03:20 +0500

    done

---
+++ day3/main.py
@@ -0,0 +1,12 @@
+name = input('enter ur name nigga: ')
+age = int(input('enter ur age nigga: '))
+
+print ('hello', name, 'next year ur age is', age+1)
+
+
+number1 = int(input('enter number 1: '))
+number2 = int(input('enter number 2: '))
+
+print('sum of numbers are', number1+number2)
+print('difference of numbers are', number1 - number2)
+print('multiplication of numbers are: ', number1 *number2)
